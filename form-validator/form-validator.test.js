dd4b872ed2c846b639619aca8c6ce0e5e351df74
Annie Xiao <annie.x981@gmail.com>
2021-07-28 19:27:37 -0400

    Add more form validator tests

--- form-validator/form-validator.test.js
+++ form-validator/form-validator.test.js
@@ -1,3 +1,4 @@
+// const { expect, test, jest } = require('@jest/globals');
 const fs = require('fs');
 const path = require('path');
 window.document.body.innerHTML = fs.readFileSync(path.resolve(__dirname, 'index.html'));
@@ -20,5 +21,26 @@ test('showError', () => {
 
   const formControl = usernameInput.parentElement;
   expect(formControl.classList.contains('error')).toBeTruthy();
+  expect(formControl.classList.contains('success')).toBeFalsy();
   expect(formControl.querySelector('small').innerText).toBe(message);
+});
+
+test('showSuccess', () => {
+  const emailInput = document.getElementById('email');
+
+  showSuccess(emailInput);
+
+  const formControl = emailInput.parentElement;
+  expect(formControl.classList.contains('success')).toBeTruthy();
+  expect(formControl.classList.contains('error')).toBeFalsy();
+});
+
+test('checkEmail shows error for invalid email', () => {
+  const emailInput = document.getElementById('email');
+  emailInput.value = 'ANNIE.Xiaoemail.com ';
+
+  checkEmail(emailInput);
+
+  const formControl = emailInput.parentElement;
+  expect(formControl.classList.contains('error')).toBeTruthy();
 });

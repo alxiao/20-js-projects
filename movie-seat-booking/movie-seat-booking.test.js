ed8d1aa809c5e1a4d5033b7f0559cca786535e15
Annie Xiao <annie.x981@gmail.com>
2021-08-01 18:32:24 -0400

    Update storeMovieData test

--- movie-seat-booking/movie-seat-booking.test.js
+++ movie-seat-booking/movie-seat-booking.test.js
@@ -7,14 +7,14 @@ const {
   storeMovieData,
 } = require('./script');
 
-test('storeMovieData', () => {
+test('storeMovieData stores data into localStorage', () => {
   const movieIndex = 2;
   const moviePrice = 20;
+  const localStorageSpy = jest.spyOn(window.localStorage.__proto__, 'setItem');
 
   storeMovieData(movieIndex, moviePrice);
 
-  expect(global.localStorage.getItem('selectedMovieIndex')).toBe(String(movieIndex));
-  expect(global.localStorage.getItem('selectedMoviePrice')).toBe(String(moviePrice));
+  expect(localStorageSpy.mock.calls).toEqual([['selectedMovieIndex', movieIndex], ['selectedMoviePrice', moviePrice]]);
 });
 
 test('updateSelectedCount', () => {

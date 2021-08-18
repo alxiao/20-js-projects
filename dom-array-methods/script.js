289915c5cdd3d641f8e24535010c35014aecfb32
Annie Xiao <annie.x981@gmail.com>
2021-08-18 11:28:46 -0400

    Add filter, sort, reduce

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -15,6 +15,9 @@ getRandomUser();
 
 addUserBtn.addEventListener('click', getRandomUser);
 doubleMoneyBtn.addEventListener('click', doubleMoney);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+sortRichestBtn.addEventListener('click', sortRichest);
+calculateWealthBtn.addEventListener('click', calculateWealth);
 
 // Fetch random user and add money
 async function getRandomUser() {
@@ -22,7 +25,6 @@ async function getRandomUser() {
   const data = await res.json();
 
   const user = data.results[0];
-
   const newUser = {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000),
@@ -42,10 +44,31 @@ function doubleMoney() {
   updateDOM();
 }
 
+// Sort users by money in descending order
+function sortRichest() {
+  data.sort((a, b) => b.money - a.money);
+  updateDOM();
+}
+
+// Filter for millionaires
+function showMillionaires() {
+  data = data.filter((person) => person.money >= 1000000);
+  updateDOM();
+}
+
+// Calculate total amount of wealth
+function calculateWealth() {
+  const total = data.reduce((acc, person) => (acc += person.money), 0);
+  console.log(total);
+
+  const wealthElement = document.createElement('div');
+  wealthElement.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(total)}</strong></h3>`;
+  main.appendChild(wealthElement);
+}
+
 // Add new object to data array
 function addData(obj) {
   data.push(obj);
-
   updateDOM();
 }
 

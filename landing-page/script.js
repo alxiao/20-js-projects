3bfdbc9e874fff76799222093c48fd482ebb421b
Annie Xiao <annie.x981@gmail.com>
2021-08-19 20:47:56 -0400

    Add modal styling

--- landing-page/script.js
+++ landing-page/script.js
@@ -1,8 +1,18 @@
 const menuBtn = document.getElementById('menu-btn');
-const menuContainer = document.querySelector('.menu-container');
-
+const signUpBtn = document.getElementById('sign-up');
+const closeBtn = document.getElementById('close');
+const modal = document.getElementById('modal');
 
 menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('show');
-  menuContainer.classList.toggle('show');
-})
+  document.body.classList.toggle('show-menu');
+});
+
+signUpBtn.addEventListener('click', () => {
+  modal.classList.add('show-modal');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('show-modal');
+});
+
+window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
